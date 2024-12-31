d0c6022392dfd39838d9d63ff49dee5c6ae41f69
Idosegev23 <130090986+Idosegev23@users.noreply.github.com>
2024-12-31 23:52:35 +0200

    Update ReservistForm.tsx

--- components/ReservistForm.tsx
+++ components/ReservistForm.tsx
@@ -7,7 +7,7 @@ import CouponModal from '@/components/CouponModal';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 // בדיקת חיבור לסופאבייס
